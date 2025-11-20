a8ceba609ce8243e8def06715728a1b0b8d8babc
Pooja Sonwane <sonwanepoojab@gmail.com>
2025-11-20 13:39:32 +0530

    backend socket cleanup

--- backend/src/socket/socket.ts
+++ backend/src/socket/socket.ts
@@ -1,27 +1,18 @@
-import type { Server as HttpServer } from "http";
-import socketManager, {
-  AuthenticatedSocket,
-  ChannelTracker,
-} from "./socketManager";
-import { chatSocket } from "./chat.socket";
-import { notificationSocket } from "./notification.socket";
-import { dataRefreshSocket } from "./dataRefresh.socket";
-import logger from "@src/utils/logger";
+import { Server as HttpServer } from "http";
+import { SocketManager } from "./socketManager";
+import ChatSocket from "./chat.socket";
+import DataRefreshSocket from "./dataRefresh.socket";
+import NotificationSocket from "./notification.socket";
 
 export const initializeSocket = (server: HttpServer): void => {
-  try {
-    socketManager.initialize(server);
-    socketManager.addRegistration(
-      (socket: AuthenticatedSocket, channelTracker: ChannelTracker) => {
-        chatSocket.registerListeners(socket, channelTracker);
-        notificationSocket.registerListeners(socket);
-        dataRefreshSocket.registerListeners(socket);
-      }
-    );
-    logger.info("Initializing Socket.IO server and all managers");
-  } catch (error) {
-    logger.error("Error initializing Socket.IO server and all managers", {
-      error: error instanceof Error ? error.message : String(error),
-    });
-  }
+  const io = SocketManager.init(server);
+
+  io.on("connection", socket => {
+    console.log("Socket connected:", socket.id);
+
+    // Attach socket modules
+    new ChatSocket(io, socket);
+    new DataRefreshSocket(io, socket);
+    new NotificationSocket(io, socket);
+  });
 };
